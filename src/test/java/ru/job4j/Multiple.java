b724efb060886ea04088ba822599010e3bd1eb7d
Admin <lion_gta@mail.ru>
2023-11-20 05:23:08 +0300

    6. Checkstyle [#4123 #461195]

--- src/test/java/ru/job4j/Multiple.java
+++ src/test/java/ru/job4j/Multiple.java
@@ -7,7 +7,7 @@ public class Multiple {
         System.out.println("Multiple 1:");
 
         for (int i = 1; i <= size; i++) {
-            System.out.println("1 * " + i + " = " + (i * 1));
+            System.out.println("1 * " + i + " = " + (i));
         }
     }
 }
